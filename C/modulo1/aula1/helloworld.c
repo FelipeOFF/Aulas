215ba8e55d9ba434e62abcd67719f864077bf25f
FelipeOFF <felipe-not@hotmail.com>
2017-08-06 22:22:55 -0300

    Adicionado a introdução

---
+++ C/modulo1/aula1/helloworld.c
@@ -0,0 +1,11 @@
+// Este código pode ser compilado com o seguinte comando:
+// gcc -Wall helloworld.c -o  helloworld
+// E pode ser executado digitando ./helloworld
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World.\n\n");
+
+    return 0;
+}
